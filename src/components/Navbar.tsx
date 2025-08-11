8d4a8ef17390fd859cf552df8f866d24dd951512
Kelia Iradukunda <iradukundakelia@gmail.com>
2025-08-11 21:00:37 +0200

    Add pages folder

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,15 +3,18 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="px-4 py-10 bg-amber-50 flex justify-center items-center">
+    <div className="">
+    <div className="px-4 py-10 bg-amber-50 flex space-x-96">
       <Link to="/">
-        <header className="text-black text-4xl">#VANLIFE</header>
+        <header className="text-black text-4xl cursor-pointer">#VANLIFE</header>
       </Link>
-      <ul className="text-gray-500 font-semibold flex space-x-3">
-        <li>About</li>
-        <li>Vans</li>
+      <ul className="text-gray-500 font-semibold flex space-x-8">
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/vans'>Vans</Link></li>
       </ul>
     </div>
+    </div>
+      
   );
 };
 
