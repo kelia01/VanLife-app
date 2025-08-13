5b40e16435443489f81fea48c60fca84567094f5
Kelia Iradukunda <iradukundakelia@gmail.com>
2025-08-13 11:58:46 +0200

    Van life about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,36 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <div className='relative w-full h-[90vh]'>
-      <img src="../public/home.png" alt="" 
-      className="absolute top-0 left-0 w-full h-full object-cover"/>
-<div className='absolute top-0 left-0 w-full h-full bg-black opacity-30'></div>
+      <div className="relative w-full h-[90vh]">
+        <img
+          src="../public/home.png"
+          alt=""
+          className="absolute top-0 left-0 w-full h-full object-cover"
+        />
+        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-30"></div>
 
-      <div className='text-white z-10 relative top-0 bottom-0 flex flex-col justify-center items-center h-full text-center px-4'>
-        <h1 className='font-bold text-4xl text-center '>You got the travel plans, we got the travel vans.</h1>
-        <p className='mb-1 block '>Add adventure to your life by joining #vanlife</p>
-        <p>Rent the perfect van to make perfect trip</p>
-      </div>
-      <button className='bg-yellow-600 text-center py-3 text-white'>Find your van</button>
+        <div className="text-white z-10 relative top-0 bottom-0 flex flex-col justify-center items-center h-full text-center px-4">
+          <h1 className="font-bold text-4xl text-center md:text-5xl">
+            You got the travel plans, we got the travel vans.
+          </h1>
+          <p className="mt-4 max-w-[21rem] text-white">
+            Add adventure to your life by joining #vanlife.
+            Rent the perfect van to make perfect trip
+          </p>
+        </div>
+        <button className="bg-orange-500 hover:bg-orange-600 text-white font-semibold rounded-md mt-8 px-8 py-3">
+          Find your van
+        </button>
       </div>
 
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,19 @@
-
+import Navbar from "../components/Navbar"
 
 const About = () => {
   return (
-    <div>About page</div>
+    <>
+      <Navbar />
+      <div className="">
+      <img src="../public/about.png" alt="" className=" top-0 left-0 w-full h-full object-cover"/>
+      </div>
+      <div className="bg-amber-100 mt-3 p-2.5 ">
+        <h1 className="font-bold text-2xl">Don't squeeze in a sedan when you could relax in a van</h1>
+        <p className="max-w-[20rem]">Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified 
+        before each trip
+        </p>
+      </div>
+    </>
   )
 }
 
